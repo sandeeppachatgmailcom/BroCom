adbd160f7a99a915aa204dcd6938e37cdd027875
sandeep <sandeepcheriyan@gmail.com>
2024-06-24 20:02:20 +0530

    hook getlogin modified.

--- src/useCases/useGetLogin.ts
+++ src/useCases/useGetLogin.ts
@@ -3,12 +3,12 @@ import { useDispatch,   } from "react-redux"
 import { login } from "../framework/ReduxStore/activeUser"
 import axiosApi from "../framework/api/axios"
 import { userApi } from "../entity/constants/api"
-import { useNavigate } from "react-router-dom"
+//mport { useNavigate } from "react-router-dom"
 
 
 
 const useGetLogin = (role:string)=>{
-    const navigate = useNavigate() 
+    //const navigate = useNavigate() 
     const dispatch = useDispatch()
      const getLogin =async ()=>{
         const tempuser = await  axiosApi.get(userApi.getlogin+`/${role}`) 
