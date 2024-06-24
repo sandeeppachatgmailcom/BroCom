91d5c9a3d657f8279a31229d847309c8ab5ca3d0
sandeep <sandeepcheriyan@gmail.com>
2024-06-24 17:40:52 +0530

    rerendering hompage getlogin hook modified removed role from dependencie of useeffect....

--- src/useCases/useGetLogin.ts
+++ src/useCases/useGetLogin.ts
@@ -1,16 +1,16 @@
-import { useEffect,   } from "react"
-import { useDispatch,   } from "react-redux"
-import { login } from "../framework/ReduxStore/activeUser"
-import axiosApi from "../framework/api/axios"
-import { userApi } from "../entity/constants/api"
-import { useNavigate } from "react-router-dom"
+// import { useEffect,   } from "react"
+// import { useDispatch,   } from "react-redux"
+// import { login } from "../framework/ReduxStore/activeUser"
+// import axiosApi from "../framework/api/axios"
+// import { userApi } from "../entity/constants/api"
+// import { useNavigate } from "react-router-dom"
 
 
 
-const useGetLogin = (role:string)=>{
-    const navigate = useNavigate() 
-    const dispatch = useDispatch()
-    // const getLogin =async ()=>{
+const useGetLogin = (_role:string)=>{
+    // const navigate = useNavigate() 
+    // const dispatch = useDispatch()
+    // // const getLogin =async ()=>{
     //     const tempuser = await  axiosApi.get(userApi.getlogin+`/${role}`) 
         
     //     if(!tempuser.data.success && Object.keys(tempuser.data).length <=2 ) navigate('/signin') 
@@ -18,9 +18,9 @@ const useGetLogin = (role:string)=>{
     //     else if(Object.keys(tempuser.data).length > 2) dispatch(login(tempuser.data))
     // }
 
-    useEffect(()=>{
-       // getLogin()
-    },[])
+    // useEffect(()=>{
+    //    // getLogin()
+    // },[])
  
 }
 
