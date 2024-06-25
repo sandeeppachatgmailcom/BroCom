14c277d584044b7ddc79e7c66dd98f3b8a8aab26
sandeep <sandeepcheriyan@gmail.com>
2024-06-25 08:55:09 +0530

    hook modified

--- src/useCases/useGetLogin.ts
+++ src/useCases/useGetLogin.ts
@@ -3,19 +3,22 @@ import { useDispatch,   } from "react-redux"
 import { login } from "../framework/ReduxStore/activeUser"
 import axiosApi from "../framework/api/axios"
 import { userApi } from "../entity/constants/api"
-//mport { useNavigate } from "react-router-dom"
+import { useNavigate } from "react-router-dom"
 
 
 
 const useGetLogin = (role:string)=>{
-    //const navigate = useNavigate() 
+    const navigate = useNavigate() 
     const dispatch = useDispatch()
      const getLogin =async ()=>{
+        if(role.length){
         const tempuser = await  axiosApi.get(userApi.getlogin+`/${role}`) 
         console.log(tempuser,'tempusertempuser')
-      //  if(!tempuser.data.success && Object.keys(tempuser.data).length <=2 ) navigate('/signin') 
+        console.log(document.cookie,'document.cookie')
+        if(!tempuser.data.success && Object.keys(tempuser.data).length <=2 ) navigate('/signin') 
             
           if(Object.keys(tempuser.data).length > 2) dispatch(login(tempuser.data))
+          }
     }
     useEffect(()=>{
         getLogin()
