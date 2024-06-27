39ce3f60e9b588bd91db948a75df76443dcb6f44
sandeep <sandeepcheriyan@gmail.com>
2024-06-27 17:29:01 +0530

    audioo recorder mock started

--- src/framework/components/trainer/VoiceRecorder.tsx
+++ src/framework/components/trainer/VoiceRecorder.tsx
@@ -91,7 +91,7 @@ useEffect(()=>{
         </div>
       ))}
       <div className='h-10  flex '>
-        {!myaudioLink ? <AudioRecorder   onRecordingComplete={handleRecordComplete} recorderControls={recorderControls} showVisualizer={true} />:""}
+        {!recordings ? <AudioRecorder   onRecordingComplete={handleRecordComplete} recorderControls={recorderControls} showVisualizer={true} />:""}
         {recaudio?.length ? (
           <button onClick={() => props.onSaveClick()} className="h-10 w-10   ms-1 flex justify-evenly rounded-full shadow-md shadow-gray-100 items-center border bg-gray-300 text-black  w-30 hover:border hover:border-blue-400 hover:text-blue-500 " >
             <BsFillFloppyFill className='text-green-800'/>
