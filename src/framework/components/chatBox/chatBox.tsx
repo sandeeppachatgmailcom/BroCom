1200705d49a0d69cadd0bf6c55ac01552bc0627e
sandeep <sandeepcheriyan@gmail.com>
2024-06-28 08:14:56 +0530

    soket fix connection

--- src/framework/components/chatBox/chatBox.tsx
+++ src/framework/components/chatBox/chatBox.tsx
@@ -36,7 +36,7 @@ import { toggleMultiUser } from "../../ReduxStore/multipleUser";
  
   useEffect(() => {
     console.log('socket cnnection estabished')
-    const newSocket = io(`https://sandeeppachat.xyz`)  
+    const newSocket = io(`https://sandeeppachat.xyz/`)  
     console.log(newSocket,'newSocketnewSocket')
     setSocket(newSocket);
     newSocket.on('connect', () => {
